f63ad34061d8b37877a652e7acf50ba7702cda5a
l29nguyen <l29nguyen@gmail.com>
2021-05-31 19:54:46 -0400

    rewrote first code, now beta is calculated for each day

--- model1.R
+++ model1.R
@@ -1,88 +1,88 @@
+
 library(dplyr)
 library(lubridate)
 library(readr)
 library(deSolve)
-library(rstudioapi)
+library(ggplot2)
+library(scales)
+library(tidyverse)
 
-directory <- dirname(rstudioapi::getSourceEditorContext()$path) # doesn't work if ran in Terminal
+#reading in dataset
 
-cases <- read.csv(file.path(directory, "data/cases_by_status_and_phu.csv"))
-cases <- cases %>% filter(PHU_NAME == "TORONTO")
-cases_sort <- cases[order(cases$FILE_DATE),]
+cases <- read.csv("C:/Users/Lena Nguyen/Desktop/school/semester 2/CHL5250 special topics/covid case study/cases_by_status_and_phu.csv")
 
+#grouping all PHU's together
 
-new_case <- read.csv(file.path(directory, "data/COVID19.csv"))
+cases_ont <- cases %>% 
+  group_by(FILE_DATE) %>% 
+  summarise(active = sum(ACTIVE_CASES), resolved = sum(RESOLVED_CASES), deaths = sum(DEATHS))
 
+#reading in dataset with new daily cases
 
-infectious_period <- 10 # from https://www.cdc.gov/coronavirus/2019-ncov/hcp/duration-isolation.html
-latent_period <- 3 #https://link.springer.com/article/10.1007/s10237-020-01332-5
-#recovery_time <- 14
+new_cases <- read.csv("C:/Users/Lena Nguyen/Desktop/school/semester 2/CHL5250 special topics/covid case study/new_cases.csv")
+new_cases$date <- as.Date(new_cases$category, format = "%a %b %d %Y")
+new_cases <- new_cases %>% filter(date >= as.Date('2020-04-01'))
 
-#Data as of 7-06
-active <- 570
-resolved <- 11791
-deaths <- 1070
-N <- 2.3 * 1000000 #population of toronto
-S <- N - active - resolved -deaths# number of susceptible individuals 
+#combining two datasets
 
-#calculating beta
-#kirkby recommends using sampling time < time it takes one individual to infect another
-#calculation for transmission rate by https://www.nature.com/articles/s41598-017-09209-x
+df <- data.frame(cases_ont, new = new_cases$New.cases)
 
-t <- 7 #sampling interval 
-Inew <- 388 #number of new infections since last sample (from 06-30 to 07-06)
-I <- 570 #number of infected individuals
+#making new variables to calculate proportions
 
-beta <- (-1/t) * log(1-Inew*((1/I)+(1/S)))
+N <- 14.57*1000000 # population of ontario
 
-#parameters
-gamma <- 1/infectious_period
-delta <- 1/latent_period
-
-#Ro <-  beta/gamma
-
-
-#code for SEIR
-
-parameter_list <- c(beta, gamma, delta)
-W <- S
-X <- I
-Y <- resolved
-Z <- N - W - X - Y 
-
-seir_model = function (current_timepoint, state_values, parameters)
-{
-  # create state variables (local variables)
-  S = state_values [1]        # susceptibles
-  E = state_values [2]        # exposed
-  I = state_values [3]        # infectious
-  R = state_values [4]        # recovered
-  
-  with ( 
-    as.list (parameters),     # variable names within parameters can be used 
-    {
-      # compute derivatives
-      dS = (-beta * S * I)
-      dE = (beta * S * I) - (delta * E)
-      dI = (delta * E) - (gamma * I)
-      dR = (gamma * I)
-      
-      # combine results
-      results = c (dS, dE, dI, dR)
-      list (results)
-    }
+df <- df %>% 
+  filter(FILE_DATE >= as.Date('2020-04-01') & FILE_DATE <= as.Date('2021-05-22')) %>%
+  mutate(
+    act.p = active / N,
+    res.p = resolved/N,
+    death.p = deaths/N,
+    new.p = new/N
   )
+
+#function to calculate beta
+
+sir.calc <- function(date, t=1, N=14570000){
+  Inew <- df[df$FILE_DATE == date, c("new")]  # number of new infections since last sample
+  I <- df[df$FILE_DATE == date, c("active")] # number of infected individuals
+  R<- df[df$FILE_DATE == date, c("resolved")] # number of recovered 
+  D<- df[df$FILE_DATE == date, c("deaths")] # number of deaths
+  S <- N - I - R - D
+  beta <- (-1/t) * log(1-Inew*((1/I)+(1/S)))
 }
 
-initial_values = c (S = W/N, E = X/N, I = Y/N, R = Z/N)
-timepoints = seq (0, 200, by=1)
-output = lsoda(initial_values, timepoints, seir_model, parameter_list)
+#make new column for beta by date
 
-plot (S ~ time, data = output, type='b', ylim = c(0,1), col = 'blue', ylab = 'S, E, I, R', main = 'SEIR epidemic') 
-par (new = TRUE)    
-plot (E ~ time, data = output, type='b', ylim = c(0,1), col = 'pink', ylab = '', axes = FALSE)
-par (new = TRUE) 
-plot (I ~ time, data = output, type='b', ylim = c(0,1), col = 'red', ylab = '', axes = FALSE) 
-par (new = TRUE)  
-plot (R ~ time, data = output, type='b', ylim = c(0,1), col = 'green', ylab = '', axes = FALSE)
-legend(140, 1, legend = c("Susceptible", "Exposed", "Infected", "Recovered"), col = c("blue", "pink", "red", "green"), lty =1, cex= 0.8)
+df$beta <- sir.calc(df$FILE_DATE)
+
+#graphing proportion active cases and beta
+
+coeff = 10
+
+ggplot(df, aes(x = FILE_DATE))+
+  geom_point(aes(y = act.p)) +
+  geom_point(aes(y = beta/coeff)) +
+  scale_y_continuous(name = 'proportion active cases', sec.axis = sec_axis(~.*coeff, name = 'beta'))+
+  theme(axis.text.x = element_text(angle = 90))+
+  geom_vline(xintercept = c('2020-09-08'), color = 'blue', size = .5) + #schools open
+  geom_vline(xintercept = c('2020-09-17'), color = 'blue', size = .5) + #gathering limits reduced
+  geom_vline(xintercept = c('2020-09-25'), color = 'blue', size = .5) + #restrictions on opening hours for bars/restaurants
+  geom_vline(xintercept = c('2020-09-28'), color = 'blue', size = .5) + #indoor dining banned
+  geom_vline(xintercept = c('2020-10-02'), color = 'blue', size = .5) + #capacity limits on bars/ gyms
+  geom_vline(xintercept = c('2020-10-10'), color = 'blue', size = .5) + #Peel/ ottawa/ toronto back to stage 2
+  geom_vline(xintercept = c('2020-10-16'), color = 'blue', size = .5) + #york moved to stage 2
+  geom_vline(xintercept = c('2020-11-06'), color = 'blue', size = .5) + #Peel moved to red tier
+  geom_vline(xintercept = c('2020-11-07'), color = 'blue', size = .5) + #Shift to 5 tier ssystem to determine restrictions
+  geom_vline(xintercept = c('2020-11-14'), color = 'blue', size = .5) + #toronto moved to red tier
+  geom_vline(xintercept = c('2020-11-16'), color = 'blue', size = .5) + # other regions moved to red tier
+  geom_vline(xintercept = c('2020-11-23'), color = 'blue', size = .5) + #all non-essential businessess closed
+  geom_vline(xintercept = c('2020-12-14'), color = 'blue', size = .5) + #york, windsor moved to lockdown
+  geom_vline(xintercept = c('2020-12-15'), color = 'blue', size = .5) + #pfizer vaccine administration begins
+  geom_vline(xintercept = c('2020-12-21'), color = 'blue', size = .5) + #hamilton moves to lockdown
+  geom_vline(xintercept = c('2020-12-26'), color = 'blue', size = .5) + #provincewide shutdown
+  geom_vline(xintercept = c('2021-01-14'), color = 'blue', size = .5) + #additional stay at home orders 
+  geom_vline(xintercept = c('2021-01-29'), color = 'blue', size = .5) + #new travel restrictions - individuals on foreign flights required to take PCR test
+  geom_vline(xintercept = c('2021-02-10'), color = 'blue', size = .5) + #stayat home orders lifted in some areas
+  geom_vline(xintercept = c('2021-03-03'), color = 'blue', size = .5) + #first AZ vaccines distributed in torotno windsor kingston
+  geom_vline(xintercept = c('2021-03-05'), color = 'blue', size = .5) + #ontario exits stay at home orders.  toronto/ peel enter lockdown
+  geom_vline(xintercept = c('2021-04-08'), color = 'blue', size = .5) #second stay at home order, vaccinations by postal codes
