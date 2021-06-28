166ea6dd12a91b10289edc05ac0d3cb095e38de1
fusehang <fusehang@hotmail.com>
2021-06-27 20:42:10 -0400

    Updates

--- sirmodel_Sumin_v2.R
+++ sirmodel_Sumin_v2.R
@@ -346,6 +346,106 @@ matplot(df2$time,df2$R,type = 'l',col='blue',ylim=c(ymin,ymax),xlab="time",ylab=
 par(new = TRUE)
 matplot(output_v2$time,output_v2$R*N,type = 'l',col='red',ylim=c(ymin,ymax),xlab="time",ylab="count")
 
+
+## Codes updated by Hana F.
+png('Predictions.png', width = 600, height = 500, pointsize = 19)
+par(mfrow=c(2, 2), oma=c(2,0,0,0), mar=c(3,3.5,2,1), mgp=c(2.75,0.8,0), font.lab=2, 
+    cex.axis=0.8, cex.lab=0.8, cex.main=1.0)
+N=14750653
+
+byMillions <- 1000000 # for scaling y-axis
+byThousands <- 1000 # for scaling y-axis
+
+# Combine df2 with output_v2 to df3
+names(output_v2) <- paste0(names(output_v2), '_est')
+df3 <- cbind(df2, output_v2)
+df3$S_N <- df3$S_est*N
+df3$E_N <- df3$E_est*N
+df3$I_N <- df3$I_est*N
+df3$R_N <- df3$R_est*N
+
+# ymax=max(df2$S,output_v2$S*N,na.rm=T)/byMillions
+# ymin=min(df2$S,output_v2$S*N,na.rm=T)/byMillions
+#xmax=mydata$date[1]
+#xmin=mydata$date[length(mydata$date)]
+ymax=max(df3[c('S','S_N')],na.rm=T)/byMillions
+ymin=min(df3[c('S','S_N')],na.rm=T)/byMillions
+
+dateNum <- as.numeric(df3$date) # for setting x-axis labeling
+xAt <- seq(dateNum[1], tail(dateNum,1), by=5) # for setting x-axis labeling
+
+matplot(df3$date,df3[c('S','S_N')]/byMillions,type = 'l', lty = 1, lwd = 1.5,
+        xaxt = 'n',yaxt = 'n',
+        col=c('blue','red'), ylim=c(ymin,ymax),
+        xlab="", ylab="Population (millions)", main="Susceptible")
+text(x = mean(dateNum), y = ymin - 0.02, labels = "Date", xpd = NA, srt = 0, cex = 0.85, font = 2)
+axis(1, at=xAt, labels = FALSE)
+text(x= xAt, y = par("usr")[3] - 0.008,
+     labels = format(as.Date(xAt), '%B %d'), 
+     xpd = NA,
+     srt = 45, ## Rotate the labels by 45 degrees.
+     cex = 0.75)
+axis(2, las = 2)
+# par(new = TRUE)
+# matplot(output_v2$time,output_v2$S*N,type = 'l',col='red',ylim=c(ymin,ymax),xlab="time",ylab="count")
+
+ymax=max(df3[c('E','E_N')],na.rm=T)
+ymin=min(df3[c('E','E_N')],na.rm=T)
+
+matplot(df3$date,df3[c('E','E_N')],type = 'l', lty = 1, lwd = 1.5,
+        xaxt = 'n',yaxt = 'n',
+        col=c('blue','red'), ylim=c(ymin,ymax),
+        xlab="", ylab="Population", main="Exposed")
+text(x = mean(dateNum), y = ymin - 3500, labels = "Date", xpd = NA, srt = 0, cex = 0.85, font = 2)
+axis(1, at=xAt, labels = FALSE)
+text(x= xAt, y = par("usr")[3] - 1500,
+     labels = format(as.Date(xAt), '%B %d'), 
+     xpd = NA,
+     srt = 45, ## Rotate the labels by 45 degrees.
+     cex = 0.75)
+axis(2, las = 2)
+
+ymax=max(df3[c('I','I_N')],na.rm=T)
+ymin=min(df3[c('I','I_N')],na.rm=T)
+
+matplot(df3$date,df3[c('I','I_N')],type = 'l', lty = 1, lwd = 1.5,
+        xaxt = 'n',yaxt = 'n',
+        col=c('blue','red'), ylim=c(ymin,ymax),
+        xlab="", ylab="Population", main="Infectious")
+text(x = mean(dateNum), y = ymin - 5500, labels = "Date", xpd = NA, srt = 0, cex = 0.85, font = 2)
+axis(1, at=xAt, labels = FALSE)
+text(x= xAt, y = par("usr")[3] - 2300,
+     labels = format(as.Date(xAt), '%B %d'), 
+     xpd = NA,
+     srt = 45, ## Rotate the labels by 45 degrees.
+     cex = 0.75)
+axis(2, las = 2)
+
+ymax=max(df3[c('R','R_N')],na.rm=T)/byThousands
+ymin=min(df3[c('R','R_N')],na.rm=T)/byThousands
+
+matplot(df3$date,df3[c('R','R_N')]/byThousands,type = 'l', lty = 1, lwd = 1.5,
+        xaxt = 'n',yaxt = 'n',
+        col=c('blue','red'), ylim=c(ymin,ymax),
+        xlab="", ylab="Population (thousands)", main="Recovered")
+text(x = mean(dateNum), y = ymin - 20, labels = "Date", xpd = NA, srt = 0, cex = 0.85, font = 2)
+axis(1, at=xAt, labels = FALSE)
+text(x= xAt, y = par("usr")[3] -8.5,
+     labels = format(as.Date(xAt), '%B %d'), 
+     xpd = NA,
+     srt = 45, ## Rotate the labels by 45 degrees.
+     cex = 0.75)
+axis(2, las = 2)
+
+# Legend
+par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
+plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
+legend("bottom", c("Observed","Estimated"), xpd = TRUE, horiz = TRUE, 
+       inset = c(0,0), bty = "n", lty = 1, lwd=2, col = c("blue","red"), cex = 0.9)
+
+dev.off()
+
+
 # RMSE calculation
 
 observed <- df2[, c('time', 'S', 'E', 'I', 'R')]
